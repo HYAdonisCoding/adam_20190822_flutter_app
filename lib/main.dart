5c83ca4f239cbb10a0ae515029e2b90e93d6ec51
曹弘扬 <296786475@qq.com>
2019-08-23 16:42:05 +0800

    image asset

--- lib/main.dart
+++ lib/main.dart
@@ -1,68 +1,12 @@
 import 'package:flutter/material.dart';
 
-class Product {
-  final String title;
-  final String description;
-  Product(this.title, this.description);
-}
+void main()=>runApp(MyApp());
 
-void main(List<String> args) {
-  runApp(MaterialApp(
-    title: 'Page Back Data',
-    home: FirstPage(),
-  ));
-}
-
-class FirstPage extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Get Phone Number'),),
-      body: Center(child: RouteButton(),) 
-    );
-  }
-}
-
-class RouteButton extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return RaisedButton(
-      onPressed: (){_navigateToLittleGile(context);},
-      child: Text('See Little Gril'),
-    );
-  }
-
-  _navigateToLittleGile(BuildContext context) async {
-    final result = await Navigator.push(context, MaterialPageRoute(
-      builder: (context) => LittleGril()
-    ));
-    Scaffold.of(context).showSnackBar(SnackBar(content: Text('${result}'),));
-  }
-}
-
-class LittleGril extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('I am a little gril.'),),
-      body: Center(
-        child: Column(
-          children: <Widget>[
-            RaisedButton(
-              child: Text('long leg little gril'),
-              onPressed: (){
-                Navigator.pop(context, 'long leg little gril, 13801234567');
-              },
-            ),
-            RaisedButton(
-              child: Text('big bar little gril'),
-              onPressed: (){
-                Navigator.pop(context, 'big bar little gril, 13901234567');
-              },
-            )
-          ],
-        ),
-      ),
+    return Container(
+      child:Image.asset('images/DAimG_2018020911628203P30T.jpg'),
     );
   }
 }
