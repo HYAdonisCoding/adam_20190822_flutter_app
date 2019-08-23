f3498c9c50290b2603d2538f45859e6bf5d0459e
曹弘扬 <296786475@qq.com>
2019-08-23 13:59:45 +0800

    Column Widget

--- lib/main.dart
+++ lib/main.dart
@@ -9,32 +9,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Row Widget Demo',
+      title: 'Columnr Widget Demo',
       home: Scaffold(
-        appBar: new AppBar(title: new Text('Row Widget')),
-        body: new Row(
+        appBar: new AppBar(title: new Text('Column Widget')),
+        body: Center(
+          child: new Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            //不灵活的
-             new RaisedButton(
-                onPressed: (){},
-                color: Colors.red,
-                child: new Text('Red'),
-              ),
-              //灵活的布局
-            Expanded(
-              child: new RaisedButton(
-                onPressed: (){},
-              color: Colors.orangeAccent,
-              child: new Text('orange Button'),
-              ),
-            ),
-            new RaisedButton(
-                onPressed: (){},
-                color: Colors.lightBlue,
-                child: new Text('lightBlue'),
-              ),
+            Text('I am adam.'),
+            Expanded(child: Text('I am web www.baidu.com.'),),
+            Text('I am coding.'),
+            Text('I am bush.'),
           ],
-        )
+        ),)
       ),
     );
   }
