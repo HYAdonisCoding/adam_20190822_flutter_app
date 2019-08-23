aef47b287620a21d332608df83223fe53fc3b574
曹弘扬 <296786475@qq.com>
2019-08-23 09:37:55 +0800

    Text Widget

--- lib/main.dart
+++ lib/main.dart
@@ -5,13 +5,34 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
+      title: 'Text Widget',
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome to Flutter'),
-        ),
         body: Center(
-          child: Text('Hello World'),
+          child: Container(
+            child: new Text(
+            'Hello Text Widget,Hello JSPang',
+            // textAlign: TextAlign.left,
+            maxLines: 1,
+            style: TextStyle(
+              fontSize: 40.0,
+              color: Color.fromARGB(255, 255, 150, 150),
+              decoration: TextDecoration.underline,
+              decorationStyle: TextDecorationStyle.solid,
+            ),
+          ),
+          alignment: Alignment.topLeft,
+          width: 500.0,
+          height: 400.0,
+          // color: Colors.lightBlue,
+          padding: const EdgeInsets.fromLTRB(10.0, 30.0, 20.0, 20.0),
+          margin: const EdgeInsets.all(10.0),
+          decoration: new BoxDecoration(
+            gradient: const LinearGradient(
+              colors: [Colors.lightBlue, Colors.greenAccent, Colors.purple]
+            ),
+            border: Border.all(width: 1.0, color: Colors.pink)
+          ),
+          ),
         ),
       ),
     );
