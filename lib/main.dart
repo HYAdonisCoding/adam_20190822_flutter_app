192410ff4fb0bd63f049e52a6881e3f422038721
曹弘扬 <296786475@qq.com>
2019-08-23 11:14:15 +0800

    ListView Widget Sample dynamic

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,27 @@
 import 'package:flutter/material.dart';
-void main() => runApp(MyApp());
+void main() => runApp(MyApp(
+  items: new List<String>.generate(1000, (i)=>"Item $i")
+));
 
 class MyApp extends StatelessWidget {
+  final List<String> items;
+
+  MyApp({Key key, @required this.items}):super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Adam Flutter Demo',
       home: Scaffold(
         appBar: new AppBar(title: new Text('ListView Widget')),
-        body: Center(
-          child: Container(
-          height: 200.0,
-          child: MyList(),
+        body: new ListView.builder(
+          itemCount: items.length,
+          itemBuilder: (context, index){
+            return new ListTile(
+              title: new Text('${items[index]}'),
+            );
+          },
         ),
-        )
       ),
     );
   }
