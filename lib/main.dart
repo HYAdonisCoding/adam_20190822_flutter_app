dd67038d921a57e77cb27b0420e3e3280cb893a7
曹弘扬 <296786475@qq.com>
2019-08-23 16:04:43 +0800

    Navigation transport params

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,55 @@
 import 'package:flutter/material.dart';
 
-void main() {
+class Product {
+  final String title;
+  final String description;
+  Product(this.title, this.description);
+}
+
+void main(List<String> args) {
   runApp(MaterialApp(
-    title: 'Navigation',
-    home: new FirstScreen(),
-    )
+    title: 'Navigation transport params',
+    home: ProductList(
+      products:List.generate(20, (i) => Product('Shopping Centre: Goods $i', 'Shopping Centre: This is a goods Detial, number: $i'))),
+    ),
   );
 }
 
-class FirstScreen extends StatelessWidget {
+class ProductList extends StatelessWidget {
+  final List<Product> products;
+  ProductList({Key key, @required this.products}):super(key: key);
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: AppBar(title: Text('Home'),),
-      body: Center(
-        child: RaisedButton(
-          child: Text('Detail Button'),
-          onPressed: (){
-            Navigator.push(context, new MaterialPageRoute(
-              builder: (context) => new SecondSreen())
-              );
-          },
-        ),
-        ),
+    return Scaffold(
+      appBar: AppBar(title: Text('Goods List')),
+      body: ListView.builder(
+        itemCount: products.length,
+        itemBuilder: (context, index){
+          return ListTile(
+            title: Text(products[index].title),
+            onTap: (){
+              Navigator.push(context, MaterialPageRoute(
+                builder: (context) => ProductDetail(product: products[index]),
+                ));
+            },
+          );
+        },
+      ),
     );
   }
 }
 
-class SecondSreen extends StatelessWidget {
+class ProductDetail extends StatelessWidget {
+  final Product product;
+
+  ProductDetail({Key key, @required this.product}):super(key:key);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Detail'),),
-      body: Center(child: RaisedButton(
-        child: Text('Back'),
-        onPressed: (){
-          Navigator.pop(context);
-        },
-      ),),
+      appBar: AppBar(title: Text('${product.title}'),),
+      body: Center(
+        child: Text('${product.description}'),
+      ),
     );
   }
 }
