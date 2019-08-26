1e417ee1195947e6fbb4ab429dfdad181a4f4d26
曹弘扬 <296786475@qq.com>
2019-08-26 15:40:45 +0800

    Bottom NavigationBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
+import 'bottom_navigation_widget.dart';
 
 void main()=>runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child:Image.asset('images/DAimG_2018020911628203P30T.jpg'),
+    return MaterialApp(
+      title: 'Flutter BottomNavigationBar',
+      theme: ThemeData.light(),
+      home: BottomNavigationWidget(),
     );
   }
 }
---
+++ lib/pages/airpaly_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AirpalyScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Airpaly'),),
+      body: Center(child: Text('Airpaly'),),
+    );
+  }
+}
