038d57e002bdc9081a855c2a81ebac424179676f
曹弘扬 <296786475@qq.com>
2019-08-27 16:45:16 +0800

    Flutter Expansion Tile

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'warp_demo.dart';
+import 'expansion_tile.dart';
 
 void main()=>runApp(MyApp());
 
@@ -7,10 +7,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Warp Layout',
+      title: 'Flutter Expansion Tile',
       //自定义主题样本
       theme: ThemeData.dark(),
-      home: WarpDemo()
+      home: ExpansionTileDemo()
     );
   }
 }
