dbaef40828799981361b781b471351e359edddf3
曹弘扬 <296786475@qq.com>
2019-08-27 17:39:56 +0800

    Flutter Custom Clipper Bessel

--- lib/custom_clipper.dart
+++ lib/custom_clipper.dart
@@ -26,6 +26,39 @@ class BottomClipper extends CustomClipper<Path> {
     var path = Path();
     path.lineTo(0, 0);
     path.lineTo(0, size.height-space);
+    var firstControlPoint = Offset(size.width*0.25, size.height); 
+    var firstEndPoint = Offset(size.width*0.44, size.height-space*0.5); 
+    path.quadraticBezierTo(firstControlPoint.dx, 
+    firstControlPoint.dy, 
+    firstEndPoint.dx, 
+    firstEndPoint.dy);
+
+    var sencondControlPoint = Offset(size.width*0.75, size.height-space*1.5); 
+    var secondEndPoint = Offset(size.width, size.height-space*0.5); 
+    
+    path.quadraticBezierTo(sencondControlPoint.dx, 
+    sencondControlPoint.dy, 
+    secondEndPoint.dx, 
+    secondEndPoint.dy);
+    path.lineTo(size.width, size.height-space);
+    path.lineTo(size.width, 0);
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    
+    return false;
+  }
+}
+
+class BottomClipperBessel extends CustomClipper<Path> {
+  final space = 60;
+  @override
+  Path getClip(Size size) {
+    var path = Path();
+    path.lineTo(0, 0);
+    path.lineTo(0, size.height-space);
     var firstControlPoint = Offset(size.width*0.5, size.height); 
     var firstEnddPoint = Offset(size.width, size.height-space); 
     path.quadraticBezierTo(firstControlPoint.dx, 
--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Custom Clipper',
+      title: 'Flutter Custom Clipper Bessel',
       debugShowCheckedModeBanner: false,
       //自定义主题样本
       theme: ThemeData.light(),
